d5829f02c8d1f0d57715b94b029ba640cd52e360
Harry <harryrock67@gmail.com>
2018-10-28 03:08:16 -0400

    Other players now see the same objects(red)

--- dev/js/environment.js
+++ dev/js/environment.js
@@ -3,11 +3,19 @@ sky = new Sky();
 
 var Environment = function () {
     this.init = () => {
+        let otherPlayersExist = true;
         addLights();
         loadGround();
         sky.init();
         zone.init();
         walls = new Wall(Math.floor(arenaSize / 2), 5, player.mesh);
         grass = new Grass((arenaSize / 2), 1);
+        if (otherPlayersExist) {
+            walls.download();
+            walls.init();
+        } else {
+            walls.init();
+            walls.upload();
+        }
     }
 }
--- dev/js/objects.js
+++ dev/js/objects.js
@@ -1,18 +1,42 @@
 var Wall = function (number, height, player) {
     this.list = [];
-    var obj_geo, obj_mat, obj, objHeight, objBorder;
-    for (let i = 0; i < number; i++) {
-        objHeight = Math.floor((Math.random() * height) + 3)
+    this.posList = [];
+    this.downloadStatus = false;
+    var obj_geo, obj_mat, obj, objHeight, objBorder, wallScope = this;
+    this.init = () => {
         obj_geo = new THREE.BoxGeometry(3, objHeight, 3, 3, 3, 3);
         obj_mat = new THREE.MeshPhongMaterial({
             color: 0x6f2c2c
         });
-        obj = new THREE.Mesh(obj_geo, obj_mat);
-        objBorder = new THREE.Mesh(obj_geo, gameStroke);
-        obj.position.set((arenaSize / 2) - (Math.floor(Math.random() * arenaSize)), objHeight / 2, (arenaSize / 2) - (Math.floor(Math.random() * arenaSize)));
-        this.list.push(obj);
-        obj.add(objBorder);
-        scene.add(obj);
+        if (this.downloadStatus == true) {
+            setTimeout(() => {
+                for (let i = 0; i < this.posList.length; i++) {
+                    obj = new THREE.Mesh(obj_geo, obj_mat);
+                    objHeight = this.posList[i].h;
+                    objBorder = new THREE.Mesh(obj_geo, gameStroke);
+                    obj.position.set(this.posList[i].x, this.posList[i].h / 2, this.posList[i].z);
+                    this.list.push(obj);
+                    obj.add(objBorder);
+                    scene.add(obj);
+                }
+            }, 300);
+        } else {
+            for (let i = 0; i < number; i++) {
+                obj = new THREE.Mesh(obj_geo, obj_mat);
+                objHeight = Math.floor((Math.random() * height) + 3)
+                objBorder = new THREE.Mesh(obj_geo, gameStroke);
+                obj.position.set((arenaSize / 2) - (Math.floor(Math.random() * arenaSize)), objHeight / 2, (arenaSize / 2) - (Math.floor(Math.random() * arenaSize)));
+                this.list.push(obj);
+                this.posList[i] = {
+                    x: obj.position.x,
+                    y: obj.position.y,
+                    z: obj.position.z,
+                    h: objHeight
+                }
+                obj.add(objBorder);
+                scene.add(obj);
+            }
+        }
     }
     this.applyPhysics = repelForce => {
         var px = player.position.x,
@@ -22,7 +46,7 @@ var Wall = function (number, height, player) {
         if (controls.sprinting == true) {
             repelForce *= 2;
         }
-        for (let i = 0; i < this.list.length; i++) {
+        for (let i in this.list) {
             if ((px - this.list[i].position.x) <= 2 && (px - this.list[i].position.x) >= -2 && (pz - this.list[i].position.z) <= 2 && (pz - this.list[i].position.z) >= -2) {
                 if (px - this.list[i].position.x <= 0) {
                     player.position.x -= repelForce;
@@ -36,6 +60,26 @@ var Wall = function (number, height, player) {
                 }
             }
         }
+    }
+    this.upload = () => {
+        console.log(this.posList);
+        for (let i in this.posList) {
+            gameRef.child(i).set({
+                x: this.posList[i].x,
+                y: this.posList[i].y,
+                z: this.posList[i].z,
+                h: this.posList[i].h
+            });
+            this.downloadStatus = false;
+        }
+    }
+    this.download = () => {
+        gameRef.on('value', data => {
+            for (let i in data.val()) {
+                wallScope.posList[i] = data.val()[i];
+            }
+        });
+        this.downloadStatus = true;
     }
 }
 
